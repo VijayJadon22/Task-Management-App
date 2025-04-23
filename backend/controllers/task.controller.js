fffa90cf91b7d3187a75c3df0d0a1e31b2840839
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-04-23 19:49:12 +0530

    Implemnted updateTask controller function

--- backend/controllers/task.controller.js
+++ backend/controllers/task.controller.js
@@ -27,14 +27,17 @@ export const readTasks = async (req, res) => {
 }
 export const updateTask = async (req, res) => {
     try {
-
+        const { id } = req.params;
+        const updatedTask = await Task.findByIdAndUpdate(id, req.body, { new: true });
+        if (!updatedTask) return res.status(404).json({ message: "Task not found" });
+        return res.send(updatedTask);
     } catch (error) {
-
+        return res.status(500).json({ message: "Error updating Task" });
     }
 }
 export const deleteTask = async (req, res) => {
     try {
-
+        
     } catch (error) {
 
     }
