0c3030d5ba8c80479695b0672bdc2189679d122d
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-04-23 19:52:36 +0530

    Implemnted authMiddleware function for checking token in user request and authenticating User

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.schema";
+
+export const authMiddleware = async (req, res, next) => {
+    try {
+        const token = req.cookies.token;
+        if (!token) return res.status(403).json({ message: "Not Authenticated" });
+
+        const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+        if (!decodedData) return res.status(404).json({ message: "Token expired" });
+
+        const user = await User.findById(decodedData.userId);
+        req.user = user;
+        next();
+    } catch (error) {
+        console.log("Error in authMiddleware", error.message);
+        return res.status(400).json({ message: "Not Authenticated" });
+    }
+}
