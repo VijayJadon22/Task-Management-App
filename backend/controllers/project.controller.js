96bfe58e760c89f3d838a5b5d069d75863875140
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-04-23 20:28:08 +0530

    Implemented deleteProject controller function successfully

--- backend/controllers/project.controller.js
+++ backend/controllers/project.controller.js
@@ -1,4 +1,5 @@
 import Project from "../models/project.schema.js";
+import Task from "../models/task.schema.js";
 
 export const createProject = async (req, res) => {
     try {
@@ -9,7 +10,7 @@ export const createProject = async (req, res) => {
         const project = new Project({
             name,
             description,
-            userId:req.user._id,
+            userId: req.user._id,
         })
         await project.save();
     } catch (error) {
@@ -21,13 +22,21 @@ export const getAllProjects = async (req, res) => {
         const projects = await Project.find({ userId: req.user._id }).populate("tasks");
         return res.status(200).send(projects);
     } catch (error) {
-        return res.status(500).json({ message: "Error fetching project" });
+        return res.status(500).json({ message: "Error fetching projects" });
     }
 }
 export const deleteProject = async (req, res) => {
     try {
-        
+        const { id } = req.params;
+        const project = await Project.findById(id);
+        if (!project || project.userId.toString() !== req.user._id) {
+            return res.status(403).send("You are not authorized to delete this project");
+        }
+
+        await Task.deleteMany({ projectId: id });
+        await Project.findOneAndDelete(id);
+        return res.status(200).json({ message: "Project deleted successfully" });
     } catch (error) {
-        
+        return res.status(500).json({ message: "Error deleteing project" });
     }
 }
