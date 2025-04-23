8217ec0948c2fdac88bcafe266c001a5519772b2
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-04-23 19:25:28 +0530

    Implemnted handleLogout controller function for users

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -35,3 +35,12 @@ export const handleLogin = async (req, res) => {
         return res.status(500).json({ message: "Error logging in user try again later" });
     }
 }
+
+export const handleLogout = async (req, res) => {
+    try {
+        res.clearCookie("token");
+        return res.status(200).json({ message: "Logged out successfully" });
+    } catch (error) {
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
