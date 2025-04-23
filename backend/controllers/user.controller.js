35404cf8ea9dd3cab5578935788b9d87b95d7f9a
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-04-23 19:22:58 +0530

    Implemnted handleLogin controller function for letting user login

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,7 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.schema.js";
+import jwt from "jsonwebtoken";
+import { generateTokenAndSetCookie } from "../utils/generateToken.js";
 export const handleSignup = async (req, res) => {
     try {
         const { name, email, password, country } = req.body;
@@ -21,7 +23,15 @@ export const handleSignup = async (req, res) => {
 export const handleLogin = async (req, res) => {
     try {
         const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) return res.status(404).json({ message: "User not found" });
+
+        const isValid = await bcrypt.compare(password, user.password);
+        if (!isValid) return res.status(404).json({ message: "Inavlid Credentials" });
+
+        await generateTokenAndSetCookie(user, res);
+        return res.status(200).json({ message: "Login successfull", user: { name: user.name, email: user.email, country: user.country } });
     } catch (error) {
-        
+        return res.status(500).json({ message: "Error logging in user try again later" });
     }
 }
