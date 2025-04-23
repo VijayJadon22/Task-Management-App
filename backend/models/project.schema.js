56f09243a93b1ca443a3d0bf5de478774092a0dd
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-04-23 20:16:03 +0530

    Implemnted Project schema for creating Projects

---
+++ backend/models/project.schema.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const projectSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    tasks: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Task",
+    }]
+}, { timestamps: true });
+
+const Project = mongoose.model("Project", projectSchema);
+export default Project;
