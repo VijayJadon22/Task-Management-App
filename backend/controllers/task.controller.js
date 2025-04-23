411790ba23b092bb2ed0fd7cea546e529d833a4a
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-04-23 20:40:12 +0530

    Made changes in task controller functions as i introduces Project and each project has nested tasks attached to it

--- backend/controllers/task.controller.js
+++ backend/controllers/task.controller.js
@@ -1,13 +1,18 @@
+import Project from "../models/project.schema.js";
 import Task from "../models/task.schema.js";
 
 export const createTask = async (req, res) => {
     try {
+        const { projectId } = req.params;
+        const project = await Project.findById(projectId);
+        if (!project || project.userId.toString() !== req.user._id) return res.status(403).json({ message: "Not authorized to add tasks to this project" });
         const { title, description, completionDate } = req.body;
         const task = new Task({
             title,
             description,
             completionDate,
-            userId: req.user._id
+            userId: req.user._id,
+            projectId
         });
 
         await task.save();
@@ -18,7 +23,10 @@ export const createTask = async (req, res) => {
 }
 export const readTasks = async (req, res) => {
     try {
-        const tasks = await Task.find({ userId: req.user._id });
+        const { projectId } = req.params;
+        const project = await Project.findById(projectId);
+        if (!project || project.userId.toString() !== req.user._id) return res.status(403).json({ message: "Not authorized to read tasks to this project" });
+        const tasks = await Task.find({ projectId });
         return res.status(200).send(tasks);
     } catch (error) {
         return res.status(500).json({ message: "Error fetching tasks" });
@@ -26,9 +34,14 @@ export const readTasks = async (req, res) => {
 }
 export const updateTask = async (req, res) => {
     try {
+        const { projectId, taskId } = req.params;
+        const project = await Project.findById(projectId);
+        if (!project || project.userId.toString() !== req.user._id) return res.status(403).json({ message: "Not authorized to update tasks to this project" });
         const { id } = req.params;
-        const updatedTask = await Task.findByIdAndUpdate(id, req.body, { new: true });
-        if (!updatedTask) return res.status(404).json({ message: "Task not found" });
+        const task = await Task.findOne({ _id: taskId, projectId });
+        if (!task) return res.status(404).json({ message: "Task not found" });
+
+        const updatedTask = await Task.findByIdAndUpdate(taskId, req.body, { new: true });
         return res.send(updatedTask);
     } catch (error) {
         return res.status(500).json({ message: "Error updating Task" });
@@ -36,9 +49,15 @@ export const updateTask = async (req, res) => {
 }
 export const deleteTask = async (req, res) => {
     try {
-        const { id } = req.params;
-        const deletedTask = await Task.findOneAndDelete(id);
-        if (!deleteTask) return res.status(404).send("Task not found");
+        const { projectId, taskId } = req.params;
+        const project = await Project.findById(projectId);
+        if (!project || project.userId.toString() !== req.user._id) return res.status(403).json({ message: "Not authorized to delete tasks to this project" });
+
+        const task = await Task.findOneAndDelete({ _id: taskId, projectId });
+        if (!task) return res.status(404).send("Task not found");
+
+        await Project.findByIdAndUpdate(projectId, { $pull: { tasks: task._id } });
+
         return res.status(200).json({ message: "Task deleted Successfully" });
     } catch (error) {
         return res.status(500).json({ message: "Error deleteing Task" });
