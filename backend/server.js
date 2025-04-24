a058edab2dfc4cd8b8de77a5c13722d92826a0ae
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-04-24 23:51:56 +0530

    Implemnted changes in server.js file

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 
-app.use("/user", userRoutes);
+app.use("/auth", userRoutes);
 app.use("/task", taskRoutes);
 app.use("/project", projectRoutes);
 
