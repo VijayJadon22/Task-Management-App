167d4db36f988c77052ef732b3d2a9680f83343b
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-04-25 19:20:21 +0530

    Some tweek adjustments

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -5,7 +5,9 @@ export const generateTokenAndSetCookie = async (user, res) => {
         const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
         res.cookie("token", token, {
             httpOnly: true,
-            secure: true,
+            // secure: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
             maxAge: 3600000
         });
     } catch (error) {
