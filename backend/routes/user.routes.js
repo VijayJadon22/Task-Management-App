d2737fcc9ffcb8b07689a548dfe4b06142811ccb
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-04-25 17:46:56 +0530

    Implemnted the route for getProfile controller function

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { handleLogin, handleLogout, handleSignup } from "../controllers/user.controller.js";
+import { handleLogin, handleLogout, handleSignup,getUserProfile } from "../controllers/user.controller.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/signup", handleSignup);
 router.post("/login", handleLogin);
 router.post("/logout", handleLogout);
+router.get("/getProfile",authMiddleware, getUserProfile);
 
 
 export default router;
