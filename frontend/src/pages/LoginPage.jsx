567bde4260175f2b614643c574ef4ccf3564595a
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-04-26 23:43:58 +0530

    Implemnted Signup and Login Page

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import toast from "react-hot-toast";
 import { useDispatch } from "react-redux";
 import { login } from "../redux/slices/authSlice";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
   const [formData, setFormData] = useState({ email: "", password: "" });
@@ -18,36 +19,39 @@ const LoginPage = () => {
     dispatch(login(formData));
   };
   return (
-    <div className="p-4 flex flex-col items-center">
-      <h1 className="text-emerald-600 font-bold text-3xl mb-4">Login Page</h1>
+    <div className="p-4 flex flex-col  items-center">
+      <h1 className="text-emerald-600 font-bold text-3xl mb-12">Login Page</h1>
       <form
         onSubmit={handleSubmit}
-        className="min-w-md mx-auto shadow-xl p-4 rounded-2xl"
+        className="min-w-md mx-auto shadow-2xl p-4 rounded-2xl"
       >
-        <label htmlFor="">Email</label>
+        <label htmlFor="">Enter Your Email</label>
         <input
           onChange={handleInputChange}
           name="email"
           type="email"
           value={formData.email}
-          placeholder="Enter your Email"
+          placeholder="eg:- @smith@email.com"
           className="block p-2 border border-gray-300 rounded mb-4 w-full"
         />
-        <label htmlFor="">Password</label>
+        <label htmlFor="">Enter Your Password</label>
         <input
           onChange={handleInputChange}
           name="password"
           type="password"
           value={formData.password}
-          placeholder="Enter Password"
+          placeholder="eg:- @example123."
           className="block p-2 border border-gray-300 rounded mb-4 w-full"
         />
+        <div className="flex items-center justify-between">
         <button
           className="bg-green-500 text-white px-4 py-2 rounded cursor-pointer"
           type="submit"
         >
           Login
-        </button>
+          </button>
+          <Link to={"/signup"} className="text-blue-600 underline">Not a memeber? create a account</Link>
+        </div>
       </form>
     </div>
   );
--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import toast from "react-hot-toast";
 import { useDispatch } from "react-redux";
 import { signup } from "../redux/slices/authSlice.js";
+import { Link } from "react-router-dom";
 
 const SignupPage = () => {
   const [formData, setFormData] = useState({
@@ -29,54 +30,59 @@ const SignupPage = () => {
     dispatch(signup(formData));
   };
   return (
-    <div className="p-4 flex flex-col items-center">
+    <div className="p-4 flex flex-col items-center justify-evenly">
       <h1 className="text-emerald-600 font-bold text-3xl mb-4">Signup Page</h1>
       <form
         onSubmit={handleSubmit}
-        className="min-w-md mx-auto shadow-xl p-4 rounded-2xl"
+        className="min-w-md mx-auto shadow-2xl p-4 rounded-2xl text-gray-700"
       >
-        <label htmlFor="name">Name</label>
+        <label htmlFor="name">Enter your Name</label>
         <input
           onChange={handleInputChange}
           name="name"
           type="text"
           value={formData.name}
-          placeholder="Enter your Name"
+          placeholder="eg: - Smith George"
           className="block p-2 border border-gray-300 rounded mb-4 w-full"
         />
-        <label htmlFor="">Email</label>
+        <label htmlFor="">Enter Your Email</label>
         <input
           onChange={handleInputChange}
           name="email"
           type="email"
           value={formData.email}
-          placeholder="Enter your Email"
+          placeholder="eg:- @smith@email.com"
           className="block p-2 border border-gray-300 rounded mb-4 w-full"
         />
-        <label htmlFor="country">Country</label>
+        <label htmlFor="country">Enter Your Country</label>
         <input
           onChange={handleInputChange}
           name="country"
           type="country"
           value={formData.country}
-          placeholder="Enter your country"
+          placeholder="eg:- India"
           className="block p-2 border border-gray-300 rounded mb-4 w-full"
         />
-        <label htmlFor="">Password</label>
+        <label htmlFor="">Enter Your Password</label>
         <input
           onChange={handleInputChange}
           name="password"
           type="password"
           value={formData.password}
-          placeholder="Enter Password"
+          placeholder="eg:- @example123."
           className="block p-2 border border-gray-300 rounded mb-4 w-full"
         />
-        <button
-          className="bg-green-500 text-white px-4 py-2 rounded cursor-pointer"
-          type="submit"
-        >
-          Login
-        </button>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-green-600 text-white px-4 py-2 rounded cursor-pointer"
+            type="submit"
+          >
+            Login
+          </button>
+          <Link to={"/login"} className="text-blue-600 underline mr-2">
+            Already a memeber? Login
+          </Link>
+        </div>
       </form>
     </div>
   );
