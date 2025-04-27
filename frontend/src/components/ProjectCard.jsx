1555f239d3124d9947eef10132d77e277879a435
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-04-27 23:49:21 +0530

    Implemnted ProjectCard Component to display Projects

--- frontend/src/components/ProjectCard.jsx
+++ frontend/src/components/ProjectCard.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { deleteProject } from "../redux/slices/projectSlice.js";
 import { useDispatch } from "react-redux";
 import toast from "react-hot-toast";
+import { Link } from "react-router-dom";
+import { MdDeleteForever } from "react-icons/md";
 
 const ProjectCard = ({ project }) => {
   const dispatch = useDispatch();
@@ -10,19 +12,22 @@ const ProjectCard = ({ project }) => {
     toast.success("Project Deleted Successfully");
   };
   return (
-    <div className="flex flex-col justify-evenly  m-2 p-4 border border-gray-300 rounded shadow-lg hover:shadow-xl transition duration-300">
-      <h3 className="text-xl font-bold mb-2">{project.name}</h3>
+    <div className="flex flex-col justify-evenly  m-2 p-4 border border-gray-300 rounded shadow-xl hover:shadow-2xl hover:scale-105 transition duration-300">
+      <h3 className="text-xl text-gray-600 font-bold mb-2">{project.name}</h3>
       <p className="text-gray-700 mb-4">{project.description}</p>
       <div className="">
         <button
           onClick={() => handleDelete(project._id)}
-          className="bg-red-500 text-white px-4 py-2 rounded cursor-pointer mr-2"
+          className="bg-red-500 text-white px-2 py-1 rounded cursor-pointer mr-2"
         >
-          Delete
+          <MdDeleteForever />
         </button>
-        <button className="bg-green-500 text-white px-4 py-2 rounded cursor-pointer">
+        <Link
+          to={`/viewtask/${project._id}`}
+          className="bg-green-500 text-white px-2 py-1 rounded cursor-pointer"
+        >
           View Tasks
-        </button>
+        </Link>
       </div>
     </div>
   );
