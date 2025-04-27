58369ea9d7b9d2514d2f248559156feb98fa5661
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-04-27 23:51:20 +0530

    Implemtned changes in task schema to store projectId it is associated with

--- backend/models/task.schema.js
+++ backend/models/task.schema.js
@@ -27,6 +27,11 @@ const taskSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
         required: true,
+    },
+    projectId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Project",
+        required: true,
     }
 }, { timestamps: true });
 
